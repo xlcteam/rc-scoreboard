e28dfa96e8e8ad5625730d399b64ce830d22e077
mr.Shu <mr@shu.io>
2013-02-13 15:27:12 +0100

    urls updated

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^/soccer', include('soccer.urls')),
+    url(r'^soccer/', include('soccer.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 

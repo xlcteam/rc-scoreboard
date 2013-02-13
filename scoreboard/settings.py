df43b91ed6bc71c2034af5158f6ca1dd1a802b00
mr.Shu <mr@shu.io>
2013-02-13 15:49:23 +0100

    static moved

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -74,6 +74,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(DIR, 'static/'),
+    os.path.join(DIR, '../soccer/static/')
 )
 
 # List of finder classes that know how to find static files in

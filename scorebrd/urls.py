7bd6fb1545582662c87659403ed76b9b4d0f6142
mr.Shu <mr@shu.io>
2013-02-16 18:02:53 +0100

    forwarding to index and logging in

--- scorebrd/urls.py
+++ scorebrd/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = patterns('scorebrd.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^login/', 'my_login', name='login'),
-    url(r'^logout/', 'my_logout', name='logout'),
+    url(r'^$', 'index', name='main_index'),
+    url(r'^login', 'my_login', name='login'),
+    url(r'^logout', 'my_logout', name='logout'),
 )

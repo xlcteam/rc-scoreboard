3d435c44b91d414b91e95b19fd33f1c23110c715
Ado Matejov <a.matejov@centrum.sk>
2013-02-24 11:13:14 +0100

    static files moved to scorebrd

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -66,7 +66,7 @@ STATIC_ROOT = os.path.join(DIR, 'staticed/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/scorebrd/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

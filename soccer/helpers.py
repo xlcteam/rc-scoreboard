8b1575bf32a07bd027dfba7ce4477ce3096422ed
mr.Shu <mr@shu.io>
2013-03-06 22:45:42 +0100

    added helpers

---
+++ soccer/helpers.py
@@ -0,0 +1,34 @@
+from django.template.loader import get_template
+import xhtml2pdf.pisa as pisa
+import cStringIO as StringIO
+
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    result = StringIO.StringIO()
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return http.HttpResponse(result.getvalue(), mimetype='application/pdf')
+    return http.HttpResponse('We had some errors<pre>%s</pre>' % (html)
+
+# code from
+# http://code.activestate.com/recipes/65200-round-robin-pairings-generator/
+def round_robin(units, sets=None):
+    """ Generates a schedule of "fair" pairings from a list of units """
+    if len(units) % 2:
+        units.append(None)
+    count    = len(units)
+    sets     = sets or (count - 1)
+    half     = count / 2
+    schedule = []
+    for turn in range(sets):
+        pairings = []
+        for i in range(half):
+            if units[i] is None or units[count-i-1] is None:
+                continue
+            pairings.append((units[i], units[count-i-1]))
+        units.insert(1, units.pop())
+        schedule.append(pairings)
+    return schedule
+

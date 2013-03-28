f22e5ae53c4e361a820bb04a32cfc43b2fd95218
mr.Shu <mr@shu.io>
2013-03-28 23:47:30 +0100

    removed Event

--- soccer/admin.py
+++ soccer/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Team, Group, Competition, Match, Event, TeamResult
+from soccer.models import Team, Group, Competition, Match, TeamResult
 
 admin.site.register(Team)
 admin.site.register(Group)
 admin.site.register(Competition)
-admin.site.register(Event)
 admin.site.register(Match)
 admin.site.register(TeamResult)

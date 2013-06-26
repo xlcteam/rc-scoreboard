7e90afbd9e1f3c6bd091d78985148d358a06514c
Oliver Quirke <oxq161@cs.bham.ac.uk>
2013-06-26 23:28:15 +0200

    Added support for picking thermal element position

--- scorebrd/static/js/mapgen-b.js
+++ scorebrd/static/js/mapgen-b.js
@@ -167,7 +167,21 @@ $(document).ready(function () {
                 }else if($(this).hasClass('victim')){
                     $(this).addClass('black-tile').removeClass('victim').children('.victim').remove();;
                 }else{
-                    $(this).addClass('victim').append('<img src="' + IMG_DIR + 'img/rescueB/victim'+Math.floor(1+Math.random()*3)+'.png" class="victim">');
+					var x = e.pageX - this.offsetLeft - 8;
+					var y = e.pageY - this.offsetTop - 113;
+					if(plusminus(y, 25, 10) && plusminus(x, 10, 10)) {
+						$(this).addClass('victim').append('<img src="' + IMG_DIR + 'img/rescueB/victim0.png" class="victim">'); // left
+					}
+					else if(plusminus(y, 25, 10) && plusminus(x, 40, 10)) {
+						$(this).addClass('victim').append('<img src="' + IMG_DIR + 'img/rescueB/victim2.png" class="victim">'); // right
+					}
+					else if(plusminus(y, 5, 5) && plusminus(x, 25, 10)) {
+						$(this).addClass('victim').append('<img src="' + IMG_DIR + 'img/rescueB/victim3.png" class="victim">'); // top
+					}
+					else if(plusminus(y, 45, 5) && plusminus(x, 25, 10)) {
+						$(this).addClass('victim').append('<img src="' + IMG_DIR + 'img/rescueB/victim1.png" class="victim">'); // bottom
+					}
+                    
                 }
             }).contextmenu(function(){
                 $('.start').remove();

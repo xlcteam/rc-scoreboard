75c05347e88a7ab98b14cd07f4a7a34f9120624a
mr.Shu <mr@shu.io>
2013-10-10 11:00:47 +0200

    correct path of static/

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -77,7 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(DIR, 'static/'),
+    os.path.abspath(os.path.join(DIR, '../scorebrd/static/')),
 )
 
 # List of finder classes that know how to find static files in

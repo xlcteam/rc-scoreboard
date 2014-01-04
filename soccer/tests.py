ec0f00fe9809ef3c84b1dbcb40101fe210e39b87
mr.Shu <mr@shu.io>
2014-01-04 23:29:13 +0100

    putting old tests back online

--- soccer/tests.py
+++ soccer/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from models import (Team, Event, Group, Competition, Match,
+from models import (Team, Group, Competition, Match,
         TeamResult, MatchSaveForm)
 from django.test.client import Client
 
@@ -17,16 +17,17 @@ from django.test.client import Client
 #           """
 #           self.assertEqual(1 + 1, 2)
 
-#   class ModelTest(TestCase):
-#       def test_models(self):
-#           self.teamA = Team.objects.create(name="testTeamA")
-#           self.teamB = Team.objects.create(name="testTeamB")
-#           self.resultA = TeamResult.objects.create(team=self.teamA)
-#           self.resultB = TeamResult.objects.create(team=self.teamB)
-#           #self.match = Match.objects.create(teamA=self.teamA, teamB=self.teamB, referee=)
 
-#           #self.group = Group.objects.create(name="testGroup", teams=[self.teamA, self.teamB, matches=[], results=[self.resultA, self.resultB])
-#           #self.competition = Competition.objects.create(name="testCompetition", groups=[self.group])
-#           #self.event = Event.objects.create(name="testEvent", competitions=[self.competition])
+class ModelTest(TestCase):
+    def test_models(self):
+        self.teamA = Team.objects.create(name="testTeamA")
+        self.teamB = Team.objects.create(name="testTeamB")
+        self.resultA = TeamResult.objects.create(team=self.teamA)
+        self.resultB = TeamResult.objects.create(team=self.teamB)
+        #self.match = Match.objects.create(teamA=self.teamA, teamB=self.teamB, referee=)
 
-#           self.assertEqual(self.teamA.name, "testTeamA")
+        #self.group = Group.objects.create(name="testGroup", teams=[self.teamA, self.teamB, matches=[], results=[self.resultA, self.resultB])
+        #self.competition = Competition.objects.create(name="testCompetition", groups=[self.group])
+        #self.event = Event.objects.create(name="testEvent", competitions=[self.competition])
+
+        self.assertEqual(self.teamA.name, "testTeamA")

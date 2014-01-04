afb6fc5c253a8f25ea9310c24aec7e1543402f1d
mr.Shu <mr@shu.io>
2014-01-04 23:35:57 +0100

    with match and everything

--- soccer/tests.py
+++ soccer/tests.py
@@ -9,6 +9,7 @@ from django.test import TestCase
 from models import (Team, Group, Competition, Match,
         TeamResult, MatchSaveForm)
 from django.test.client import Client
+from django.contrib.auth.models import User
 
 #   class SimpleTest(TestCase):
 #       def test_basic_addition(self):
@@ -23,12 +24,19 @@ class ModelTest(TestCase):
     def setUp(self):
         self.teamA = Team.objects.create(name="testTeamA")
         self.teamB = Team.objects.create(name="testTeamB")
+
         self.resultA = TeamResult.objects.create(team=self.teamA)
         self.resultB = TeamResult.objects.create(team=self.teamB)
 
+        ref = User.objects.create_user(username='joe', email='doe',
+                password='none')
+        self.match = Match.objects.create(teamA=self.teamA, teamB=self.teamB,
+                referee=ref)
+
+        self.group = Group.objects.create(name="testGroup", teams=[self.teamA,
+            self.teamB, matches=[self.match], results=[self.resultA, self.resultB])
+
     def test_models(self):
-        #self.match = Match.objects.create(teamA=self.teamA, teamB=self.teamB, referee=)
-        #self.group = Group.objects.create(name="testGroup", teams=[self.teamA, self.teamB, matches=[], results=[self.resultA, self.resultB])
         #self.competition = Competition.objects.create(name="testCompetition", groups=[self.group])
         #self.event = Event.objects.create(name="testEvent", competitions=[self.competition])
 
